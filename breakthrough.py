1250d2e077d4bb24e799b88b1c15e427095b68ec
Morgan Nyman <57505931+mogggen@users.noreply.github.com>
2021-08-02 14:20:31 +0200

    gg

---
+++ breakthrough.py
@@ -0,0 +1,10 @@
+countCommands = 0
+bfVocabulary = "[]<>+-.,"
+mapSizeInBytzes = 8
+
+writeHeadSymbols = input("")
+readHeadSymbols = input("")
+
+loadScriptAtReadHeadIndex = scriptbuffer[0]
+
+readCommandFrame(bfVocabulary)
